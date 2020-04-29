77cc2344e127b705bf123ca7ba51826e211e2ef0
Thibault YOU <thibaultyou@gmail.com>
2020-04-29 20:16:29 +0200

    :lipstick: Updating screensaver options

--- system/desktop-screen-saver.sh
+++ system/desktop-screen-saver.sh
@@ -9,6 +9,9 @@ echo "Updating screen saver settings ..."
 # Start after begin idle for time (in seconds)
 defaults -currentHost write com.apple.screensaver idleTime -int 300
 
+# Set Flurry as screensaver
+defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName Flurry path /System/Library/Screen\ Savers/Flurry.saver/ type 0
+
 # Show with clock
 defaults -currentHost write com.apple.screensaver showClock -bool true
 
